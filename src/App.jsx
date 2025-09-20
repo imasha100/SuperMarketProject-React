84cc7ffdbbcc6b74801f4c80d339c5f49b97d55d
Imasha100 <imashamadushanikarunathilaka@gmail.com>
2025-09-20 07:42:13 +0530

    change App,jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
 import { useState } from 'react'
 import './App.css'
-import Navbar from "./components/Navbar";   
+import Navbar from "./components/Navbar";  
+import HeroSection from './components/HeroSection'; 
+import Feature from './components/Feature';
 
 function App() {
   const [count, setCount] = useState(0)
@@ -9,6 +11,8 @@ function App() {
     <div>
       {/* Navbar/Header */}
       <Navbar />
+      <HeroSection/>
+      <Feature/>
 
       
       
